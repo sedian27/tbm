b9777f449dbf8b648ff5104ad0c1493816b9ebbf
sedian <diestrasergio@gmail.com>
2022-02-23 12:18:47 -0500

    validData and validRole added

--- backend/middlewares/user.js
+++ backend/middlewares/user.js
@@ -1,5 +1,18 @@
 import User from "../models/user.js";
 
+const validData = (req, res, next) => {
+  if (!req.body.name || !req.body.email || !req.body.password)
+    return res.status(400).send({ message: "Incomplete data" });
+
+  next();
+};
+
+const validRole = (req, res, next) => {
+  if (!req.body.role)
+    return res.status(400).send({ message: "Incomplete data" });
+  next();
+};
+
 const existingUser = async (req, res, next) => {
   const existingUser = await User.findOne({ email: req.body.email });
   return existingUser
@@ -7,4 +20,4 @@ const existingUser = async (req, res, next) => {
     : next();
 };
 
-export default { existingUser };
+export default { existingUser, validData, validRole };
