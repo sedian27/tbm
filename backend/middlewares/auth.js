17060093a2433007eb955762b8776339cc257dc1
sedian <diestrasergio@gmail.com>
2022-02-23 12:17:55 -0500

    delete comments

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,10 @@
 import jwt from "jsonwebtoken";
-//     0        1
-// Bearer, asddfgfegrgsdfg445gsvsd)
+
 const auth = async (req, res, next) => {
   let token = req.header("Authorization");
   if (!token)
     return res.status(400).send({ message: "Authorization denied: No token" });
 
-    //asddfgfegrgsdfg445gsvsd
   token = token.split(" ")[1];
   if (!token)
     return res.status(400).send({ message: "Authorization denied: No token" });
